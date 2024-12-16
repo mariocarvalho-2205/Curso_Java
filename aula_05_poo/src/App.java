12962e972e97eacdd292165fe364783e6ae7ec27
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-12-16 20:45:47 -0300

    feat: intro a poo

---
+++ aula_05_poo/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Carro c = new Carro();
+    }
+}
+
+class Carro {
+    public Carro(){
+        System.out.println("Carro");
+
+    }
+}

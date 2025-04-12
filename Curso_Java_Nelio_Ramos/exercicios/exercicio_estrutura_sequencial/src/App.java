e5c879104cf61d9177548e44d7f02d1bab94952c
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2025-04-12 12:19:40 -0300

    fix: exercicio 3 finalizado

--- Curso_Java_Nelio_Ramos/exercicios/exercicio_estrutura_sequencial/src/App.java
+++ Curso_Java_Nelio_Ramos/exercicios/exercicio_estrutura_sequencial/src/App.java
@@ -8,30 +8,82 @@ public class App {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
         // Exercicio 01
-        System.out.println("Exercicio 01 - Soma de dois valores.");
-        System.out.println("Digite dois valores para a soma");
-        int num1, num2, soma;
+        // System.out.println("Exercicio 01 - Soma de dois valores.");
+        // System.out.println("Digite dois valores para a soma");
+        // int num1, num2, soma;
+
+        // System.out.println("Digite o primeiro valor:");
+        // num1 = scanner.nextInt();
+        // System.out.println("Digite o segundo valor:");
+        // num2 = scanner.nextInt();
+
+        // soma = num1 + num2;
+        // System.out.printf("A soma de %d + %d é %d\n", num1, num2, soma);
+
+        // // Exercicio 02
+        // System.out.println("Exercicio 02 - Area do Circulo");
+        // double raio, area, pi;
+        // pi = 3.14159;
+        // System.out.println("Digite o raio do circulo:");
+        // raio = scanner.nextDouble();
+        // area = pi * Math.pow(raio, 2);
+        // // calculo da area do circulo
+
+        // System.out.printf("A area do circulo é %.4f\n", area);
+
+        /*
+         * Exercicio 03 - Calculo da diferença
+         * Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir,
+         * calcule e mostre a diferença do produto
+         * de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C *
+         * D).
+         */
+
+        // int vr1, vr2, vr3, vr4, diferenca;
+
+        // System.out.println("Didite 4 valores para saber a diferença do calculo entre eles");
         
-        System.out.println("Digite o primeiro valor:");
-        num1 = scanner.nextInt();
-        System.out.println("Digite o segundo valor:");
-        num2 = scanner.nextInt();
-        
-        soma = num1 + num2;
-        System.out.printf("A soma de %d + %d é %d\n", num1, num2, soma);
-        
-        // Exercicio 02
-        System.out.println("Exercicio 02 - Area do Circulo");
-        double raio, area, pi;
-        pi = 3.14159;
-        System.out.println("Digite o raio do circulo:");
-        raio = scanner.nextDouble();
-        area = pi * Math.pow(raio, 2);
-        // calculo da area do circulo
-        
-        System.out.printf("A area do circulo é %.4f\n", area);
-        
-        
+        // System.out.println("Didite o 1 valor.");
+        // vr1 = scanner.nextInt();
+        // System.out.println("Didite o 2 valor.");
+        // vr2 = scanner.nextInt();
+        // System.out.println("Didite o 3 valor.");
+        // vr3 = scanner.nextInt();
+        // System.out.println("Didite o 4 valor.");
+        // vr4 = scanner.nextInt();
+
+        // diferenca = (vr1 * vr2) - (vr3 * vr4);
+        // System.out.printf("A diferença %d * %d - %d * %d é de %d", vr1, vr2, vr3, vr4, diferenca);
+
+        /*
+         * Exercicio 04
+         * Fazer um programa que leia o número de um funcionário, seu número de horas
+         * trabalhadas, o valor que recebe por
+         * hora e calcula o salário desse funcionário. A seguir, mostre o número e o
+         * salário do funcionário, com duas casas
+         * decimais.
+         */
+
+        /*
+         * Exercicio 05
+         * Fazer um programa para ler o código de uma peça 1, o número de peças 1, o
+         * valor unitário de cada peça 1, o
+         * código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2.
+         * Calcule e mostre o valor a ser pago.
+         */
+
+        /*
+         * Exercicio 06
+         * Fazer um programa que leia três valores com ponto flutuante de dupla
+         * precisão: A, B e C. Em seguida, calcule e
+         * mostre:
+         * a) a área do triângulo retângulo que tem A por base e C por altura.
+         * b) a área do círculo de raio C. (pi = 3.14159)
+         * c) a área do trapézio que tem A e B por bases e C por altura.
+         * d) a área do quadrado que tem lado B.
+         * e) a área do retângulo que tem lados A e B.
+         */
+
         scanner.close();
     }
 }
